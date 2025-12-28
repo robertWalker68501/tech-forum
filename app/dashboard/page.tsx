53d90a806361688caa6656c2584a84f01e788cbf
robert <robert.walker6580@gmail.com>
2025-12-27 16:56:04 -0800

    implement protect dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,21 @@
+'use client';
+
+import { useUser } from '@clerk/nextjs';
+
 import PageHeader from '@/components/shared/PageHeader';
 
 const Dashboard = () => {
+  const { isSignedIn, user, isLoaded } = useUser();
+
+  // Handle loading state
+  if (!isLoaded) return <div>Loading...</div>;
+
+  // Protect the page from unauthenticated users
+  if (!isSignedIn) return <div>Sign in to view this page</div>;
+
   return (
     <div className='container'>
-      <PageHeader title='Dashboard' />
+      <PageHeader title={`${user.firstName}&apos;s dashboard`} />
     </div>
   );
 };
